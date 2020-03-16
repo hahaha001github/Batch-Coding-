575e244893737037ed7dab90e9cdec9fb54835d0
hahaha001github <60880612+hahaha001github@users.noreply.github.com>
2020-03-16 12:05:59 +0900

    Create new file

---
+++ yaaaaaaaay.bat
@@ -0,0 +1,8 @@
+@echo off 
+mode 10 
+color 0a 
+
+:A 
+%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%
+goto A 
+
